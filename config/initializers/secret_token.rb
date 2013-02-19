0a5582193ed47b05c3bba241f1bcc6cd1ecdd3bc
Alexander Shapkin <as.zgroupsoft@gmail.com>
2013-02-19 16:08:10 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GithubNotifications::Application.config.secret_token = '5efd0e983539361b298bf586598b74c9634944e6547f59a70162deb725838542bea59a9d6ca2e768107f1f2fda1c8e4e52f89b027db0260b7e6b96e36142b11f'
